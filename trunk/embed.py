e318ca5be31c0752e49597b770a2c851d48177c5
rantav <rantav@24706afc-f8fe-11dd-9d1a-b59b2e1864b6>
2009-03-09 14:05:00 +0000

    - improve embed code! - several JS improvements.

--- trunk/embed.py
+++ trunk/embed.py
@@ -11,10 +11,19 @@ import index
 
 class EmbedHandler(index.MainHandler):
 
-	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'embed.js')
-		self.response.out.write(template.render(path, self.get_template_values()))
+  def get(self):
+    template_values = self.get_template_values();
+    template_values['base_url'] = 'http://%s' % self.get_host_name()
+    self.response.headers['Content-Type'] = 'application/javascript; charset=utf-8'
+    path = os.path.join(os.path.dirname(__file__), 'embed.js')
+    self.response.out.write(template.render(path, template_values))
 
+  def get_host_name(self):
+    if os.environ.get('HTTP_HOST'):
+      url = os.environ['HTTP_HOST']
+    else:
+      url = os.environ['SERVER_NAME']
+    return url
 
 def main():
   application = webapp.WSGIApplication([('/embed', EmbedHandler)],
