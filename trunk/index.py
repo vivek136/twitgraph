5442f8c046a4d187e892b07198ec036c2c76ec17
rantav <rantav@24706afc-f8fe-11dd-9d1a-b59b2e1864b6>
2009-03-11 15:23:17 +0000

    Add file version to js and css files to prevent version conflics on live.

--- trunk/index.py
+++ trunk/index.py
@@ -10,36 +10,36 @@ from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
 
-	def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, self.get_template_values()))
-	
-	def get_template_values(self):
-		r = self.request
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, self.get_template_values()))
 
-		q = r.get('q')
-		if not q:
-			q = 'youtube annotations'
+  def get_template_values(self):
+    r = self.request
 
-		if r.get('dynamic_date') == '0':
-			dynamic_date = False
-		else:
-			dynamic_date = True
+    q = r.get('q')
+    if not q:
+      q = 'youtube annotations'
 
-		duration = r.get('duration')
-		if not duration:
-			duration = 7
+    if r.get('dynamic_date') == '0':
+      dynamic_date = False
+    else:
+      dynamic_date = True
 
-		template_values = {
-			'q': q,
-			'dynamic_date': dynamic_date,
-			'show_text': r.get('show_text') == '1',
-			'duration': duration or 0,
-			'start': r.get('start'),
-			'end': r.get('end'),
+    duration = r.get('duration')
+    if not duration:
+      duration = 7
 
-		}
-		return template_values
+    template_values = {
+      'q': q,
+      'dynamic_date': dynamic_date,
+      'show_text': r.get('show_text') == '1',
+      'duration': duration or 0,
+      'start': r.get('start'),
+      'end': r.get('end'),
+      'version': os.environ['CURRENT_VERSION_ID'],
+    }
+    return template_values
 
 
 
